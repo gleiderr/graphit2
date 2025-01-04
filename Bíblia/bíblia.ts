f86e4f0afb5d2d7ddee7a77db2b24df19d0c11e9
Gleider Ribeiro Gonçalves Costa <gleider.costa@unimedbh.com.br>
2025-01-04 10:36:17 -0300

    Atualiza a lógica de inserção de versículos

--- Bíblia/bíblia.ts
+++ Bíblia/bíblia.ts
@@ -1,13 +1,21 @@
 import { graphit } from '../Graphit';
+import { graphit2 } from '../Graphit2';
 
 class Bíblia {
   inserirVersículo(referência: string, versículo: string, definirArestas: () => void) {
+    graphit2.aresta([referência, versículo]);
+    const setReferência = graphit2.addListener('afterAresta', id => {
+      graphit2.aresta([{ id }, 'Referência', '1 Rs 15.33']);
+    });
+
     graphit.inserirAresta([referência, versículo, 'Texto']);
     graphit.iniciarMemória();
     definirArestas();
     graphit.getMemória().forEach(([aresta]) => {
       graphit.inserirAresta([aresta, referência, 'Referência']);
     });
+
+    graphit2.removeListener('afterAresta', setReferência);
   }
 }
 
