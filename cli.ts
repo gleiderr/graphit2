79b818d4a7ae3aaec1490a0067883a0486a66c22
Gleider Ribeiro Gonçalves Costa <gleider.costa@unimedbh.com.br>
2025-02-08 19:00:20 -0300

    Refatora CLI para melhorar a interação
    - Transforma funções em métodos da classe CLI.
    - Adiciona tratamento de exclusão de itens.
    - Atualiza a lógica de renderização e seleção de expressões.
    - Inclui novo arquivo a ser vigiado para nodemon.

--- cli.ts
+++ cli.ts
@@ -1,91 +1,123 @@
 import console from 'console';
 import readline from 'readline';
-import { outputDB } from './Biblia';
+import { inputDB } from './Biblia';
 import { graphit } from './Graphit';
 
-let selectedIndex = 0;
-let expressaoId: string;
-let items: string[] = [];
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-graphit.carregar(outputDB);
-
-const promptExpressaoId = () => {
-  rl.question('Digite o Id da expressão que deseja reordenar: ', id => {
-    expressaoId = id;
-    const expressao = graphit.get(expressaoId);
-    items = expressao.expressões;
-    renderList();
-    listenForKeyPress();
+class CLI {
+  private selectedIndex = 0;
+  private expressaoId: string = '';
+  private items: string[] = [];
+  private rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-};
+  private message: string = '';
 
-const renderList = () => {
-  console.clear();
-  console.log(
-    'Use ↑ ↓ para selecionar, ctrl + ↑ ↓ para mover e Enter para sair\n',
-  );
-
-  const maxItemsPerColumn = 15;
-  const numColumns = Math.ceil(items.length / maxItemsPerColumn);
-  const columns = Array.from({ length: numColumns }, (_, i) =>
-    items.slice(i * maxItemsPerColumn, (i + 1) * maxItemsPerColumn),
-  );
-
-  for (let i = 0; i < maxItemsPerColumn; i++) {
-    let row = '';
-    columns.forEach((column, colIndex) => {
-      const item = column[i] || '';
-      const indicator =
-        i + colIndex * maxItemsPerColumn === selectedIndex ? '>' : ' ';
-      row += `${indicator} ${item}\t`;
-    });
-    console.log(row);
-  }
-};
-
-const moveSelection = (direction: 'up' | 'down') => {
-  const origem = selectedIndex;
-  let destino = selectedIndex;
-
-  if (direction === 'up' && selectedIndex > 0) {
-    destino--;
-  } else if (direction === 'down' && selectedIndex < items.length - 1) {
-    destino++;
+  constructor(private fileDB: string) {
+    graphit.carregar(this.fileDB);
+    this.promptExpressaoId();
   }
 
-  if (origem !== destino) {
-    graphit.moverExpressão(expressaoId, origem, destino);
-    graphit.salvar(outputDB);
-    selectedIndex = destino;
+  private promptExpressaoId() {
+    this.rl.question(
+      'Digite o Id do elemento que deseja reordenar as expressões que o contém: ',
+      id => {
+        this.expressaoId = id;
+        const expressao = graphit.get(this.expressaoId);
+        this.items = [...expressao.expressões];
+        this.renderList();
+        this.listenForKeyPress();
+      },
+    );
   }
-};
 
-const listenForKeyPress = () => {
-  readline.emitKeypressEvents(process.stdin);
-  if (process.stdin.isTTY) {
-    process.stdin.setRawMode(true);
-  }
+  private renderList() {
+    console.clear();
+    console.log(
+      'Use ↑ ↓ para selecionar, shift + ↑ ↓ para mover, del para excluir e Enter para retornar à seleção do elemento\n',
+    );
 
-  process.stdin.on('keypress', (_, key) => {
-    if (key.shift && key.name === 'up') {
-      moveSelection('up');
-    } else if (key.shift && key.name === 'down') {
-      moveSelection('down');
-    } else if (key.name === 'up') {
-      selectedIndex = Math.max(selectedIndex - 1, 0);
-    } else if (key.name === 'down') {
-      selectedIndex = Math.min(selectedIndex + 1, items.length - 1);
-    } else if (key.name === 'return') {
-      console.log('\nLista final:', items);
-      process.exit();
+    const maxItemsPerColumn = 10;
+    const numColumns = Math.ceil(this.items.length / maxItemsPerColumn);
+    const columns = Array.from({ length: numColumns }, (_, i) =>
+      this.items.slice(i * maxItemsPerColumn, (i + 1) * maxItemsPerColumn),
+    );
+
+    for (let i = 0; i < maxItemsPerColumn; i++) {
+      let row = '';
+      columns.forEach((column, colIndex) => {
+        const item = column[i] || '';
+        const indicator =
+          i + colIndex * maxItemsPerColumn === this.selectedIndex ? '>' : ' ';
+        row += `${indicator} ${item}\t`;
+      });
+      console.log(row);
     }
-    renderList();
-  });
-};
 
-promptExpressaoId();
+    console.log(this.message);
+  }
+
+  private moveSelection(direction: 'up' | 'down') {
+    const origem = this.selectedIndex;
+    let destino = this.selectedIndex;
+
+    if (direction === 'up' && this.selectedIndex > 0) {
+      destino--;
+    } else if (
+      direction === 'down' &&
+      this.selectedIndex < this.items.length - 1
+    ) {
+      destino++;
+    }
+
+    if (origem !== destino) {
+      graphit.moverExpressão(this.expressaoId, origem, destino);
+      graphit.salvar(this.fileDB);
+      this.selectedIndex = destino;
+    }
+  }
+
+  private deleteSelectedItem() {
+    const itemId = this.items[this.selectedIndex];
+    graphit.excluirExpressão(itemId);
+    this.items.splice(this.selectedIndex, 1);
+    this.selectedIndex = Math.min(this.selectedIndex, this.items.length - 1);
+    graphit.salvar(this.fileDB);
+    this.message = `Item ${itemId} excluído com sucesso.`;
+  }
+
+  private listenForKeyPress() {
+    readline.emitKeypressEvents(process.stdin);
+    if (process.stdin.isTTY) {
+      process.stdin.setRawMode(true);
+    }
+
+    process.stdin.on('keypress', (_, key) => {
+      if (key.shift && key.name === 'up') {
+        this.moveSelection('up');
+      } else if (key.shift && key.name === 'down') {
+        this.moveSelection('down');
+      } else if (key.name === 'up') {
+        this.selectedIndex = Math.max(this.selectedIndex - 1, 0);
+      } else if (key.name === 'down') {
+        this.selectedIndex = Math.min(
+          this.selectedIndex + 1,
+          this.items.length - 1,
+        );
+      } else if (key.name === 'delete') {
+        try {
+          this.deleteSelectedItem();
+        } catch (error) {
+          if (error instanceof Error) {
+            this.message = error.message;
+          }
+        }
+      } else if (key.name === 'return' || key.name === 'escape') {
+        this.promptExpressaoId();
+      }
+      this.renderList();
+    });
+  }
+}
+
+new CLI(inputDB);
