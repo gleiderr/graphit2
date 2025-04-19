47097d47a1c12f26fbb79f87bfff28858fc2e291
Gleider Ribeiro Gonçalves Costa <gleider.costa@unimedbh.com.br>
2025-04-19 10:54:20 -0300

    ✨ Volta a implementar fluxo útil
    - Cria o arquivo estatisticas.json com dados iniciais.
    - Cria o arquivo Baasa2.md com informações sobre Baasa.
    - Atualiza o script principal para salvar estatísticas.
    - Implementa a função para formatar a data e hora.

--- index.ts
+++ index.ts
@@ -1,39 +1,49 @@
-import { bíblia, outputDB } from './Biblia';
-import { graphit } from './Graphit';
-const agora: string = new Date().toLocaleString(); //
-console.log(`----------------------------------${agora}`);
+import { readFileSync, writeFileSync } from 'fs';
+import { Graphit } from './Graphit';
+import { Markdown } from './Markdown';
 
-import './inserções/1 Reis/1 Rs 14.6, 14.14';
-import './inserções/1 Reis/1 Rs 15.16-22';
-import './inserções/1 Reis/1 Rs 15.27-29';
-import './inserções/1 Reis/1 Rs 15.32-34';
-import './inserções/1 Reis/1 Rs 16.1-13';
-import './inserções/1 Reis/1 Rs 21.17-22';
-import './inserções/2 Crônicas/2 Cr 16.1-6';
-import './inserções/2 Reis/2 Rs 9.6,9';
-import './inserções/Jeremias/Jr 41.9';
+// TODO: Diferenciar "Deus" de "deus".
+// TODO: Extrair dados direto da Bíblia.
 
-bíblia.imprimirEstudos({
-  foco: ['Baasa'],
-  outros: [
-    'Acabe',
-    'Aías',
-    'Asa',
-    'Ben-Hadade',
-    'Elá',
-    'Elias',
-    'Geba',
-    'Gibetom',
-    'Ismael',
-    'Israel',
-    'Jeroboão',
-    'Jeú',
-    'Tabriom',
-    'Tirza',
-  ],
-  pronto: [],
+console.log('Iniciando.............................................' + agora());
+// Abre arquivo JSON de estatísticas
+const estatisticas = JSON.parse(readFileSync('./estatisticas.json', 'utf-8'));
+const dadosAgora = estatisticas[hora()] || (estatisticas[hora()] = {});
+
+const graphit = new Graphit();
+const markdown = new Markdown(graphit);
+markdown.ler(readFileSync('./estudos/foco/Baasa.md', 'utf-8'));
+
+writeFileSync(
+  './estudos/foco/Baasa2.md',
+  markdown.escrever([graphit.informação('Baasa')]),
+  'utf-8'
+);
+
+// Salva estatísticas em arquivo JSON.
+dadosAgora.termos = 0;
+dadosAgora.expressões = 0;
+dadosAgora.observações =
+  'Dados extraídos do arquivo Baasa.md criado antes desta nova implementação.';
+graphit.índices.forEach(indice => {
+  const informação = graphit.get(indice);
+  if ('termos' in informação) dadosAgora.expressões++;
+  else dadosAgora.termos++;
 });
 
-bíblia.imprimeNãoVisitados();
+console.log(estatisticas);
+const estatisticasString = JSON.stringify(estatisticas, null, 2);
+writeFileSync('./estatisticas.json', estatisticasString, 'utf-8');
+console.log('Finalizando...........................................' + agora());
 
-graphit.salvar(outputDB);
+function hora() {
+  const data = agora();
+  return data.substring(0, 14) + ':00';
+}
+
+function agora() {
+  // Formato DD/MM/YYYY HH:mm
+  return new Date().toLocaleString('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
+  });
+}
