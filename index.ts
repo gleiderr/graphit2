c10c29d9d3be4119f193d7ae1eba2908d594499a
Gleider Ribeiro Gonçalves Costa <gleider.costa@unimedbh.com.br>
2025-05-03 10:13:44 -0300

    🚧 Melhora impressão de estatísticas
    Inclui dados de escrita em Baasa2.md filtrando já visitados.
    Implementa função para executar com estatísticas.
    Refatora lógica de salvamento de estatísticas em JSON.

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from 'fs';
-import { Expressão, Graphit, Id, Termo } from './Graphit';
-import { Esquema, esquemaPadrão, Markdown } from './Markdown';
+import { Graphit, Termo } from './Graphit';
+import { esquemaPadrão, Markdown } from './Markdown';
 
 // TODO: Tratar diferença entre "Deus" de "deus" e permitir registro de termo como mais de um valor válido
 // TODO: - Ideia 1: Incluir no preambulo do arquivo markdown os termos com mais de um valor válido.
@@ -16,69 +16,89 @@ import { Esquema, esquemaPadrão, Markdown } from './Markdown';
 
 // TODO: Implementar escrita dos textos que contém o termo alvo.
 
-const início = getAgora();
-console.log('Iniciando...................................' + início.formatado);
-// Abre arquivo JSON de estatísticas
-const estatisticas = JSON.parse(readFileSync('./estatisticas.json', 'utf-8'));
-const horaAtual = hora();
-const dadosAgora = estatisticas[horaAtual] || (estatisticas[horaAtual] = {});
+executarComEstatísticas(
+  'Escreve em Baasa2.md, dentro de comentário, textos relacionados a Baasa filtrando os já visitados.',
+  () => {
+    const graphit = new Graphit();
+    const markdown = new Markdown(graphit);
+    markdown.ler(readFileSync('./estudos/foco/Baasa2.md', 'utf-8'));
+    markdown.ler(readFileSync('./estudos/Bíblia.md', 'utf-8'));
 
-const graphit = new Graphit();
-const markdown = new Markdown(graphit);
-markdown.ler(readFileSync('./estudos/foco/Baasa2.md', 'utf-8'));
-markdown.ler(readFileSync('./estudos/Bíblia.md', 'utf-8'));
+    const termoBaasa = graphit.informação('Baasa') as Termo;
+    const baasa = markdown.escrever([termoBaasa]);
 
-const termoBaasa = graphit.informação('Baasa') as Termo;
-const baasa = markdown.escrever([termoBaasa]);
+    const baasaPertence_a = termoBaasa.pertence_a
+      .filter(i => !markdown.visitados.has(i))
+      .map(i => graphit.get(i));
+    const pertencimento = markdown.escrever(baasaPertence_a, {
+      ...esquemaPadrão,
+      nívelInicial: 2,
+    });
 
-const baasaPertence_a = termoBaasa.pertence_a
-  .filter(i => !markdown.visitados.has(i))
-  .map(i => graphit.get(i));
-const pertencimento = markdown.escrever(baasaPertence_a, {
-  ...esquemaPadrão,
-  nívelInicial: 2,
-});
+    writeFileSync(
+      './estudos/foco/Baasa2.md',
+      [
+        baasa,
+        '<!-- Outros textos a que Baasa pertence\n',
+        pertencimento,
+        '-->',
+      ].join('\n'),
+      'utf-8'
+    );
 
-writeFileSync(
-  './estudos/foco/Baasa2.md',
-  [
-    baasa,
-    '<!-- Outros textos a que Baasa pertence\n',
-    pertencimento,
-    '-->',
-  ].join('\n'),
-  'utf-8'
+    let termos = 0;
+    let expressões = 0;
+    graphit.índices.forEach(indice => {
+      const informação = graphit.get(indice);
+      if ('termos' in informação) expressões++;
+      else termos++;
+    });
+
+    return { termos, expressões };
+  }
 );
 
-// Salva estatísticas em arquivo JSON.
-dadosAgora.duração = (Date.now() - início.time) / 1000; // Duração em segundos como número
-dadosAgora.termos = 0;
-dadosAgora.expressões = 0;
-dadosAgora.observações =
-  'Escreve em Baasa2.md, dentro de comentário, textos relacionados a Baasa filtrando os já visitados.';
-graphit.índices.forEach(indice => {
-  const informação = graphit.get(indice);
-  if ('termos' in informação) dadosAgora.expressões++;
-  else dadosAgora.termos++;
-});
+function executarComEstatísticas(
+  observações: string,
+  funcao: () => { termos: number; expressões: number }
+) {
+  const início = getAgora();
+  console.log(
+    'Iniciando...................................' + início.formatado
+  );
+  // Abre arquivo JSON de estatísticas
+  const estatisticas = JSON.parse(readFileSync('./estatisticas.json', 'utf-8'));
+  const horaAtual = hora();
+  const dadosAgora = estatisticas[horaAtual] || (estatisticas[horaAtual] = {});
 
-console.log(estatisticas[horaAtual]);
-const estatisticasString = JSON.stringify(estatisticas, null, 2);
-writeFileSync('./estatisticas.json', estatisticasString, 'utf-8');
-console.log('Finalizando..............................' + getAgora().formatado);
+  const { termos, expressões } = funcao(); // Executa a função passada como argumento
 
-function hora() {
-  const data = getAgora();
-  return data.formatado.substring(0, 14) + ':00';
-}
+  // Salva estatísticas em arquivo JSON.
+  dadosAgora.duração = (Date.now() - início.time) / 1000; // Duração em segundos como número
+  dadosAgora.termos = termos;
+  dadosAgora.expressões = expressões;
+  dadosAgora.observações = observações;
 
-function getAgora() {
-  const dataHoraAtual = new Date();
-  // Formato DD/MM/YYYY HH:mm
-  return {
-    formatado: dataHoraAtual.toLocaleString('pt-BR', {
-      timeZone: 'America/Sao_Paulo',
-    }),
-    time: dataHoraAtual.getTime(),
-  };
+  console.log(estatisticas[horaAtual]);
+  const estatisticasString = JSON.stringify(estatisticas, null, 2);
+  writeFileSync('./estatisticas.json', estatisticasString, 'utf-8');
+  console.log(
+    'Finalizando..............................' + getAgora().formatado
+  );
+
+  function hora() {
+    const data = getAgora();
+    return data.formatado.substring(0, 14) + ':00';
+  }
+
+  function getAgora() {
+    const dataHoraAtual = new Date();
+    // Formato DD/MM/YYYY HH:mm
+    return {
+      formatado: dataHoraAtual.toLocaleString('pt-BR', {
+        timeZone: 'America/Sao_Paulo',
+      }),
+      time: dataHoraAtual.getTime(),
+    };
+  }
 }
