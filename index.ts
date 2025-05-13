8503481e5a47a56906f909da202be0f73639a476
Gleider Ribeiro Gonçalves Costa <gleider.costa@unimedbh.com.br>
2025-05-13 08:01:10 -0300

    Reflete novas definições

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { readFileSync, writeFileSync } from 'fs';
 import { esquemaBíblia } from './Biblia';
 import { Graphit, Termo } from './Graphit';
 import { esquemaPadrão, Markdown } from './Markdown';
+import { Tokenizer } from './Tokenizer';
 
 // TODO: Tratar diferença entre "Deus" de "deus" e permitir registro de termo como mais de um valor válido
 // TODO: - Ideia 1: Incluir no preambulo do arquivo markdown os termos com mais de um valor válido.
@@ -17,44 +18,87 @@ import { esquemaPadrão, Markdown } from './Markdown';
 
 // TODO: Implementar escrita dos textos que contém o termo alvo.
 
-executarComEstatísticas('Obtém versículos para cada linha escrita', () => {
-  const graphit = new Graphit();
-  const markdown = new Markdown(graphit);
-  markdown.ler(readFileSync('./estudos/foco/Baasa2.md', 'utf-8'));
-  markdown.ler(readFileSync('./estudos/Bíblia.md', 'utf-8'));
+executarComEstatísticas(
+  'Torna termos minúsculos por padrão e lida com substantivos próprios',
+  () => {
+    const tokenizer = new Tokenizer();
 
-  const termoBaasa = graphit.informação('Baasa') as Termo;
-  const baasa = markdown.escrever([termoBaasa], esquemaBíblia);
+    // Lugares
+    tokenizer.addNomesPróprios(
+      'Benjamim',
+      'Geba',
+      'Gibetom',
+      'Israel',
+      'Judá',
+      'Mispá',
+      'Ramá',
+      'Tirza'
+    );
 
-  const baasaPertence_a = termoBaasa.pertence_a
-    .filter(i => !markdown.escritos.has(i))
-    .map(i => graphit.get(i));
-  const pertencimento = markdown.escrever(baasaPertence_a, {
-    ...esquemaPadrão,
-    nívelInicial: 2,
-  });
+    // Pessoas
+    tokenizer.addNomesPróprios(
+      'Acabe',
+      'Aías',
+      'Asa',
+      'Baasa',
+      'Ben-Hadade',
+      'Deus',
+      'Elá',
+      'Gedalias',
+      'Hanani',
+      'Ismael',
+      'Issacar',
+      'Jeroboão',
+      'Jeú',
+      'Nadabe',
+      'Nebate',
+      'Netanias',
+      'Senhor',
+      'Zinri'
+    );
 
-  writeFileSync(
-    './estudos/foco/Baasa2.md',
-    [
-      baasa,
-      '<!-- Outros textos a que Baasa pertence\n',
-      pertencimento,
-      '-->',
-    ].join('\n'),
-    'utf-8'
-  );
+    // Livros
+    tokenizer.addNomesPróprios('1Rs', '2Rs', '2Cr', 'Jr');
+    tokenizer.addNomesPróprios('1 Reis', '2 Reis', '2 Crônicas', 'Jeremias');
 
-  let termos = 0;
-  let expressões = 0;
-  graphit.índices.forEach(indice => {
-    const informação = graphit.get(indice);
-    if ('termos' in informação) expressões++;
-    else termos++;
-  });
+    const graphit = new Graphit(tokenizer);
+    const markdown = new Markdown(graphit);
+    markdown.ler(readFileSync('./estudos/foco/Baasa2.md', 'utf-8'));
+    markdown.ler(readFileSync('./estudos/Bíblia.md', 'utf-8'));
 
-  return { termos, expressões };
-});
+    const termoBaasa = graphit.informação('Baasa') as Termo;
+    const baasa = markdown.escrever([termoBaasa], esquemaBíblia);
+
+    const baasaPertence_a = termoBaasa.pertence_a
+      .filter(i => !markdown.escritos.has(i))
+      .map(i => graphit.get(i));
+    const pertencimento = markdown.escrever(baasaPertence_a, {
+      ...esquemaPadrão,
+      nívelInicial: 2,
+    });
+
+    writeFileSync(
+      './estudos/foco/Baasa2.md',
+      [
+        baasa,
+        '<!-- Outros textos a que Baasa pertence\n',
+        pertencimento,
+        '-->',
+      ].join('\n'),
+      'utf-8'
+    );
+
+    let termos = 0;
+    let expressões = 0;
+    graphit.índices.forEach(indice => {
+      const informação = graphit.get(indice);
+      if ('termos' in informação) expressões++;
+      else termos++;
+    });
+
+    return { termos, expressões };
+  }
+);
 
 function executarComEstatísticas(
   observações: string,
